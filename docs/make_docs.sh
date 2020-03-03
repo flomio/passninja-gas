79ddaf585628ae140279c1a9df6eb31a66a3aee3
Andres Weber <andresmweber@gmail.com>
2020-03-03 14:27:29 -0500

    Removed make_docs and added husky to the devDependencies for pre-commit/push hooks.

--- docs/make_docs.sh
+++
@@ -1,5 +0,0 @@
-for f in *.gs; do mv -- "$f" "${f%.gs}.js"; done
-jsdoc2md --files util.js config.js > docs/UTILS.md
-jsdoc2md --files handleGet.js handlePost.js > docs/API.md
-jsdoc2md --files core.js build.js services.js> docs/FUNCTIONS.md
-for f in *.js; do mv -- "$f" "${f%.js}.gs"; done
